014a5f9bb36a40ca6d1572f51cd6595a9a6543cc
Ali Dogus Temizsoy <dogustemizsoy@gmail.com>
2021-02-27 06:42:55 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+import time
+
+# Screen Properties
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Pong Game")
+screen.tracer(0)
+
+
+screen.update()
+
+screen.mainloop()
